013a245fd452590f64dcc779587a2069d8e2a116
langdonglei <langdonglei@sina.com>
2020-02-28 20:04:35 +0800

    1

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -1,66 +1,94 @@
-// pages/calc/calc.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    result: '0',
+    roman1: 'I',
+    roman2: 'V',
+    roman3: 'X',
+    roman4: 'L',
+    roman5: 'C',
+    roman6: 'D',
+    roman7: 'M',
+    roman8: 'Del',
+    roman9: '=',
+    arab1: '1',
+    arab2: '5',
+    arab3: '10',
+    arab4: '50',
+    arab5: '100',
+    arab6: '500',
+    arab7: '1000',
+    flag: '1',
+    fontsize: false,
+    lettermaxlength: 6
+  },
+
+  btnletter(e) {
+    let v = e.target.id;
+    let s = this.data.result;
+    let r = '';
+    if (s.length < this.data.lettermaxlength) {
+      if (s == '0') {
+        r = v;
+      } else {
+        r = s + v;
+      }
+      this.setData({ result: r })
+    }
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  btndelete() {
+    let s = this.data.result;
+    let r = '';
+    if (s.length > 0 && s.length < 7) {
+      if (s.length == 1) {
+        r = '0'
+      } else {
+        r = s.substring(0, s.length - 1);
+      }
+      this.setData({ result: r })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  btnEqual() {
+    var strs = this.data.result;
+    var currentVal = 0;
+    var previousVal = 0;
+    if (strs != '(*^_^*)' && this.data.flag == '0') {
+      var total = 0;
+      for (var i = 0; i < strs.length; i++) {
+        currentVal = this.getValue(strs.charAt(i));
+        total += currentVal;
+        previousVal = this.getValue(strs.charAt(i - 1));
+        if (i > 0 && currentVal > previousVal) {
+          total = total - 2 * this.getValue(strs.charAt(i - 1));
+        }
+      }
+      this.setData({
+        result: total,
+        flag: '1',
+        fontsize: false
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getValue(r) {
+    switch (r) {
+      case 'I':
+        return 1;
+      case 'V':
+        return 5;
+      case 'X':
+        return 10;
+      case 'L':
+        return 50;
+      case 'C':
+        return 100;
+      case 'D':
+        return 500;
+      case 'M':
+        return 1000;
+    }
   }
+
 })
