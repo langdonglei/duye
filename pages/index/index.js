92bb8b50569822d5f8c3e011cd4c51a6fedf5454
langdonglei <langdonglei@sina.com>
2020-02-28 15:01:58 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,16 @@
 const app = getApp()
 
 Page({
+  pagegan: function () {
+    wx.navigateTo({
+      url: '/pages/gan/gan',
+    })
+  },
+  pagecalc: function () {
+    wx.navigateTo({
+      url: '/pages/calc/calc',
+    });
+  },
   data: {
     motto: 'Hello World',
     userInfo: {},
@@ -10,7 +20,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +31,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,7 +53,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
