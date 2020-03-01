ba3518e062c75b620d8b9d069565892e687cdbb1
langdonglei <langdonglei@sina.com>
2020-03-01 12:35:29 +0800

    test

--- pages/gan/gan.js
+++ pages/gan/gan.js
@@ -1,66 +1,11 @@
-// pages/gan/gan.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  view1Click: function (event) {
+    console.log("view1Click");
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  view2Click: function () {
+    console.log("view2Click");
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  view3Click: function (event) {
+    console.log('view3Click');
   }
-})
+});
